69f40d7390e91f40ce3fe4eb900962e4de5ca1fd
Mauriza Humaira <81746446+MaurizaHumaira@users.noreply.github.com>
2021-12-07 12:19:26 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,56 @@
+/**
+  * Merupakan Class yang akan menjalankan seluruh program yang telah dibangun sebelumnya 
+  *
+  * @author Mauriza Humaira
+  * @version 1.0
+  */
 public class Main {
+    /**
+      * Method yang menjalankan seluruh program
+      * @param args mengambil argumen yang diberikan melalui commadnline
+      */
     public static void main(String[] args) throws Exception {
-
+    
+        // pembuatan objek observable dan observer
         Observable machine = new Machine();
         Observer cust1 = new Customer("Mauriza");
         Observer cust2 = new Customer("Ihza");
-
+        
+        // menambahkan data observer
         machine.addObserver(cust1);
         machine.addObserver(cust2);
 
         System.out.println("\n ============ Morija Printing ============\n");
-
+        
         System.out.println("\t[[[" + cust1.getName() + "'s Order]]]");
+        // membuat objek dekorasi mug bertipe papa mama
         Mug papaMamaMug = new DecoratorPapaMama(new WhiteMug(), cust1);
         papaMamaMug.make();
-
+        
+        // menampilkan animasi load
         animation();
 
         System.out.println("\n");
 
         System.out.println("\t[[[" + cust2.getName() + "'s Order]]]");
+        // membuat dekorasi bertipe ayam jago
         Mug ayamJagoMug = new DecoratorAyamJago(new WhiteMug(), cust2);
         ayamJagoMug.make();
-
+        
+        // menampilkan animasi load
         animation();
+        // menghapus layar
         clearScreen();
-
+        
+        // menampilkan notifikasi
         System.out.println("\n\n\n~~~~ Notification from : Snappy Printing ~~~~\n");
         machine.notify(" Your Mug Has Been Printed <3");
 
     }
-
+    
+    /**
+      * method untuk menjalankan animasi loading 
+      */
     public static void animation() throws Exception {
         String anim = "|/-\\";
         for (int x = 0; x <= 100; x++) {
@@ -38,7 +59,10 @@ public class Main {
             Thread.sleep(10);
         }
     }
-
+    
+    /**
+      * method yang digunakan untuk mengapus layar
+      */
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
